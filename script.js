06ad722b26510bbea55e5e79fa645f18f0f4db1b
callbackKid <vladislav.yarosh23@gmail.com>
2023-10-16 01:02:43 +0400

    project boilerplate

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('.product-form-container')
+const nameInput = document.querySelector('#name')
+const priceInput = document.querySelector('#price')
+
+let currentProducts = JSON.parse(localStorage.getItem('items')) || []
+
+form.addEventListener('submit', (e) => {
+  // предотвращаем перезагрузку страницы
+  e.preventDefault()
+  //формируем товар(объект из инпутов)
+  const newProduct = {
+    name: nameInput.value,
+    price: priceInput.value,
+  }
+  // пушим в массив
+  currentProducts.push(newProduct)
+  //очищаем поля ввода
+  nameInput.value = ''
+  priceInput.value = ''
+  // сохраняем массив в localStorage
+  localStorage.setItem('items', JSON.stringify(currentProducts))
+})
